b540a0cbe9b538afe926d40d2ebe24836e10ea9c
Akash <akash@akash.com>
2020-04-02 22:54:11 +0530

    custom webpack config

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+console.log('webpack config ran')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
